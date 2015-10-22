b7078cfc4dc055256c3baf5334d4b76639afca50
Akash Manohar J <akash@akash.im>
2015-10-22 21:01:08 +0530

    debug env

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 function run_build {
   local heroku_stack=$1
   local otp_version=$2
@@ -5,14 +7,15 @@ function run_build {
   echo "HEROKU STACK: $heroku_stack"
   echo "OTP VERSION: $otp_version"
 
-  docker build -t otp-build build-scripts -f $heroku_stack.dockerfile
-  docker run -t -e OTP_VERSION=$otp_version --name=otp-build-${otp_version}-${heroku_stack} otp-build-${heroku_stack}
+  sudo docker build -t otp-build build-scripts -f $heroku_stack.dockerfile
+  sudo docker run -t -e OTP_VERSION=$otp_version --name=otp-build-${otp_version}-${heroku_stack} otp-build-${heroku_stack}
 
-  docker cp otp-build-${otp_version}-${heroku_stack}:/home/build/out/OTP-${otp_version}.tar.gz otp-builds/OTP-${otp_version}-${heroku_stack}.tar.gz
+  sudo docker cp otp-build-${otp_version}-${heroku_stack}:/home/build/out/OTP-${otp_version}.tar.gz otp-builds/OTP-${otp_version}-${heroku_stack}.tar.gz
   ls builds/otp
 }
 
 mkdir -p builds/otp
 head -n1 otp-versions
+env
 echo "HEROKU STACK is $HEROKU_STACK"
 run_build $HEROKU_STACK $(head -n1 otp-versions)
